dcd97d2ff13585a59b3563755b5b2905e4c10b6e
programacao-git <laralima013@gmail.com>
2025-03-16 00:23:51 -0300

    Update Cartas Super Trunfo.c
    Desafio finalizado!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,73 +1,95 @@
 #include <stdio.h>
 int main() {
+  
+    //carta 1
+  char estado1[20];
+  char codigo1[20];
+  char cidade1[20];
+  int populacao1;
+  float area1;
+  float PIB1;
+  int turisticos1;
 
-int estado;
-char código[20];
-char cidade[20];
-int população;
-float área;
-float PIB;
-int turisticos;
 
-printf("carta 1");
-printf("digite o estado: \n");
-scanf("%s", estado);
+   //carta 2
+  char estado2[20];
+  char codigo2[20];
+  char cidade2[20];
+  int populacao2;
+  float area2;
+  float PIB2;
+  int turisticos2;
 
-printf("digite o código da carta: \n");
-scanf("%s", &código);
 
-printf("digite o nome da cidade: \n");
-scanf("%s", cidade);
+    //cadastro: carta 1
+   printf("cadastro da carta 1\n");
+   printf("digite o estado 1(a-h): \n");
+   scanf("%s", estado1);
 
-printf("digite a população: \n");
-scanf("%d", &população);
+   printf("digite o código da carta 1: \n");
+   scanf("%s", codigo1);
 
-printf("digite a área: \n");
-scanf("%f", &área);
+   printf("digite o nome da cidade 1: \n");
+   scanf("%s", cidade1);
 
-printf("digite o PIB: \n");
-scanf("%d", &PIB);
+   printf("digite a população 1: \n");
+   scanf("%d", &populacao1);
 
-printf("digite o número de pontos turísticos: \n");
-scanf("%d", &turisticos);
+   printf("digite a área em km2 1: \n");
+   scanf("%f", &area1);
 
-printf(" o nome do estado é: %s\n" , estado);
-printf("o código da carta é: %s\n" , código);
-printf("o nome da cidade é: %s\n" , cidade);
-printf("a população é: %d\n" , população);
-printf("a área é: %f\n" , área);
-printf("o PIB é: %d\n" , PIB);
-printf("o número de pontos turisticos é: %d\n" , turisticos);
+   printf("digite o PIB 1: \n");
+   scanf("%f", &PIB1);
 
-printf("carta 2");
-printf("digite o estado: \n");
-scanf("%s", estado);
+   printf("digite o número de pontos turísticos 1: \n");
+   scanf("%d", &turisticos1);
 
-printf("digite o código da carta: \n");
-scanf("%s", &código);
 
-printf("digite o nome da cidade: \n");
-scanf("%s", cidade);
+    //cadastro: carta 2
+   printf("cadastro da carta 2\n");
+   printf("digite o estado 2(a-h): \n");
+   scanf("%s", estado2);
 
-printf("digite a população: \n");
-scanf("%d", &população);
+   printf("digite o código da carta 2: \n");
+   scanf("%s", codigo2);
 
-printf("digite a área: \n");
-scanf("%f", &área);
+   printf("digite o nome da cidade 2: \n");
+   scanf("%s", cidade2);
 
-printf("digite o PIB: \n");
-scanf("%d", &PIB);
+   printf("digite a população 2: \n");
+   scanf("%d", &populacao2);
 
-printf("digite o número de pontos turísticos: \n");
-scanf("%d", &turisticos);
+   printf("digite a área em km2 2: \n");
+   scanf("%f", &area2);
 
-printf(" o nome do estado é: %s\n" , estado);
-printf("o código da carta é: %s\n" , código);
-printf("o nome da cidade é: %s\n" , cidade);
-printf("a população é: %d\n" , população);
-printf("a área é: %f\n" , área);
-printf("o PIB é: %d\n" , PIB);
-printf("o número de pontos turisticos é: %d\n" , turisticos);
+   printf("digite o PIB 2: \n");
+   scanf("%f", &PIB2);
 
-    return 0;
+   printf("digite o número de pontos turísticos 2: \n");
+   scanf("%d", &turisticos2);
+
+
+    //carta 1
+   printf("**dados da carta 1**\n");
+   printf("estado: %s\n" , estado1);
+   printf("código: %s\n" , codigo1);
+   printf("cidade: %s\n" , cidade1);
+   printf("população: %d\n" , populacao1);
+   printf("área em km2: %.2f\n" , area1);
+   printf("PIB: %.2f\n" , PIB1);
+   printf("pontos turisticos: %d\n" , turisticos1);
+
+
+    //carta 2
+   printf("**dados da carta 2**\n");
+   printf("estado: %s\n" , estado2);
+   printf("código: %s\n" , codigo2);
+   printf("cidade: %s\n" , cidade2);
+   printf("população: %d\n" , populacao2);
+   printf("área em km2: %.2f\n" , area2);
+   printf("PIB: %.2f\n" , PIB2);
+   printf("pontos turisticos: %d\n" , turisticos2);
+
+   return 0;
+
 }
